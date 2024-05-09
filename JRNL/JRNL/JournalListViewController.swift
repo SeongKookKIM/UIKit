bfb6f97d1c746918049abce5ec49aa2941533805
SeongKookKIM <ksk8646@naver.com>
2024-05-09 15:26:50 +0900

    TableViewDataSource, TableViewDelegate

--- JRNL/JRNL/JournalListViewController.swift
+++ JRNL/JRNL/JournalListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class JournalListViewController: UIViewController {
+class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var tableView: UITableView!
     
@@ -15,7 +15,14 @@ class JournalListViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // 실제 데이터는 엄청 많지만 테이블에게는 몇번쨰 인지 알려줘야함
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10 // 10개이다.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+    }
 }
 
