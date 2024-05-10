e2d6099492351527721cc80b4b4e6c05325ca75c
SeongKookKIM <ksk8646@naver.com>
2024-05-10 11:15:59 +0900

    JournalEntry Model Class

---
+++ JRNL/JRNL/JournalListScreen/Model/JournalEntry.swift
@@ -0,0 +1,35 @@
+//
+//  JournalEntry.swift
+//  JRNL
+//
+//  Created by mac on 5/10/24.
+//
+
+import UIKit
+
+class JournalEntry {
+    // Mark: - Properties
+    
+    let date: Date
+    let rating: Int
+    let entryTitle: String
+    let entryBody: String
+    let photo: UIImage?
+    let latitude: Double?
+    let longitude: Double?
+    
+    init?(date: Date, rating: Int, title: String, body: String, photo: UIImage? = nil, latitude: Double? = nil, longitude: Double? = nil) {
+        
+        if title.isEmpty || body.isEmpty || rating < 0 || rating > 5 {
+            return nil
+        }
+        
+        self.date = Date()
+        self.rating = rating
+        self.entryTitle = title
+        self.entryBody = body
+        self.photo = photo
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
