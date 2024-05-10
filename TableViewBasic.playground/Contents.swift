a6704a2b3ba1687a013ededf198166ddd8958904
SeongKookKIM <ksk8646@naver.com>
2024-05-10 10:18:04 +0900

    TableViewDataSource

--- TableViewBasic.playground/Contents.swift
+++ TableViewBasic.playground/Contents.swift
@@ -1,11 +1,28 @@
 import UIKit
 import PlaygroundSupport //UIKit을 플레이그라운드에서 확인 가능하게함
 
-class TableViewExampleController: UIViewController {
+class TableViewExampleController: UIViewController, UITableViewDataSource {
+    
     var tableView: UITableView?
     var journalEntries: [[String]] = [
         ["sun.max", "9 Apr 2023", "Nice weather today"],
         ["cloud.rain", "10 Apr 2023", "Heavy rain today"],
         ["cloud.sun", "11 APr 2023", "It's cloudy out"]
     ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        journalEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let journalEntry = journalEntries[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.image = UIImage(systemName: journalEntry[0])
+        content.text = journalEntry[1]
+        content.secondaryText = journalEntry[2]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 }
