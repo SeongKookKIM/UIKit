5a5bee871e09900bbaad171518ba43630595c303
SeongKookKIM <ksk8646@naver.com>
2024-05-13 11:07:46 +0900

    View ButtonItem Add

--- JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
+++ JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
@@ -8,25 +8,37 @@
 import UIKit
 
 class AddJournalViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "New Entry"
         view.backgroundColor = .white
-
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancle))
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func save() {
+        // Todo:
     }
-    */
-
+    
+    @objc func cancle() {
+        dismiss(animated: true)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
