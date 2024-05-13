c90f6435972a020b3489c54e228ba51ffc4ea25e
SeongKookKIM <ksk8646@naver.com>
2024-05-13 10:17:02 +0900

    Add TableView on JournalListViewController

--- JRNL-Codeonly/JRNL-Codeonly/JournalListViewController.swift
+++ JRNL-Codeonly/JRNL-Codeonly/JournalListViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class JournalListViewController: UIViewController {
+        
+    // tableView 초기화
+    lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        return tableView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.backgroundColor = .white
+        
+        let global = view.safeAreaLayoutGuide
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: global.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: global.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: global.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: global.leadingAnchor)
+        ])
+
     }
 
 
