c26c4887f980e5e2c0393409a79b84c9a96b17e2
SeongKookKIM <ksk8646@naver.com>
2024-05-14 14:38:52 +0900

    MKAnnotation 추가

--- JRNL/JRNL/JournalListScreen/Model/JournalEntry.swift
+++ JRNL/JRNL/JournalListScreen/Model/JournalEntry.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import MapKit
 
-class JournalEntry {
+class JournalEntry: NSObject, MKAnnotation {
+
+    
     // MARK: - Properties
     
     let date: Date
@@ -32,6 +35,22 @@ class JournalEntry {
         self.photo = photo
         self.latitude = latitude
         self.longitude = longitude
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        guard let lat = latitude, let long = longitude else {
+            return CLLocationCoordinate2D()
+        }
+        
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+    
+    var title: String? {
+        date.formatted(.dateTime.year().month().day())
+    }
+    
+    var subtitle: String? {
+        entryTitle
     }
 }
 
