d7f179161f3be3b44dd7af1866cb4de3f2fc14b9
SeongKookKIM <ksk8646@naver.com>
2024-05-16 11:13:03 +0900

    Load JSON Object from file

--- JRNL/JRNL/Model/SharedData.swift
+++ JRNL/JRNL/Model/SharedData.swift
@@ -3,7 +3,7 @@
 
 import UIKit
 
-// 싱글톤 패턴 
+// 싱글톤 패턴
 class SharedData {
     static let shared = SharedData()
     private var journalEntries: [JournalEntry]
@@ -31,5 +31,22 @@ class SharedData {
 
     func removeJournalEntry(index: Int) {
         journalEntries.remove(at: index)
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadJournalEntriesData() {
+        let pathDirectory = getDocumentDirectory()
+        let fileURL = pathDirectory.appendingPathComponent("journalEntries.json")
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let journalEntriesData = try JSONDecoder().decode([JournalEntry].self, from: data)
+            journalEntries = journalEntriesData
+        } catch {
+            print("Fail to read JSON data: \(error.localizedDescription)")
+        }
     }
 }
