795b2a1b6dd92f0e8559cafb960ecd90ab29dc7e
SeongKookKIM <ksk8646@naver.com>
2024-05-16 09:23:27 +0900

    Add SharedData

---
+++ JRNL/JRNL/Model/SharedData.swift
@@ -0,0 +1,35 @@
+
+
+
+import UIKit
+
+
+class SharedData {
+    static let shared = SharedData()
+    private var journalEntries: [JournalEntry]
+
+    private init() {
+        journalEntries = []
+    }
+
+    func numberOfJournalEntries() -> Int {
+        journalEntries.count
+    }
+
+    func getJournalEntry(index: Int) -> JournalEntry {
+        journalEntries[index]
+    }
+
+    func getAllJournalEntries() -> [JournalEntry] {
+        let readOnlyJournalEntries = journalEntries
+        return readOnlyJournalEntries
+    }
+
+    func addJournalEntry(newJournalEntry: JournalEntry) {
+        journalEntries.append(newJournalEntry)
+    }
+
+    func removeJournalEntry(index: Int) {
+        journalEntries.remove(at: index)
+    }
+}
